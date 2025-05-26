fd398f1c652ca68bc095972153e63578efa1fd67
aliem-r <ali.e.m@outlook.com>
2025-05-26 16:48:49 +0200

    Change Header font and icon weight

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import { IconSquareRoundedPlus2 } from "@tabler/icons-react";
 export default function Header() {
     return (
         <header className="py-12 flex justify-around">
-            <div className="flex items-center gap-2 text-2xl text-neutral-700">
-                <IconSquareRoundedPlus2 className="size-7" />
+            <div className="flex items-center gap-2 font-medium text-2xl text-neutral-700">
+                <IconSquareRoundedPlus2 stroke={2.5} className="size-7" />
                 <h1>Local to-do list</h1>
             </div>
         </header>

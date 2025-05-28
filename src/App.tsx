600c7e03ccb5d1a020d8d0d8c922cf150f3b96bd
aliem-r <ali.e.m@outlook.com>
2025-05-28 17:27:11 +0200

    Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TodoListWrapper from "./components/TodoListWrapper";
 
@@ -6,6 +7,7 @@ export default function App() {
         <>
             <Header />
             <TodoListWrapper />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-center items-center gap-2 text-sm font-[400] text-neutral-500 py-12">
+            <span>Project repo:</span>
+            <a
+                href="https://github.com/aliem-r/local-todo-app"
+                target="_blank"
+                className="text-neutral-400 hover:text-green-400 transition duration-100"
+            >
+                @aliem-r
+            </a>
+        </footer>
+    );
+}

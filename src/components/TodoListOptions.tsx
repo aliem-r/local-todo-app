acb18392b5a00d5cd398013d1c2a6105f4155ac6
aliem-r <ali.e.m@outlook.com>
2025-05-30 11:52:21 +0200

    Add hold to delete interaction to TodoListOptions

--- src/components/TodoListOptions.tsx
+++ src/components/TodoListOptions.tsx
@@ -1,6 +1,6 @@
 import { IconSquareRoundedLetterI, IconTrash } from "@tabler/icons-react";
+import { memo, useRef, useState } from "react";
 import { cn } from "../utils";
-import { memo } from "react";
 
 type TodoListOptionsProps = {
     hasCompletedTasks?: boolean;
@@ -13,28 +13,79 @@ export default memo(function TodoListOptions({
     onClearCompleted,
     onClearTodoList,
 }: TodoListOptionsProps) {
+    const [activeButton, setActiveButton] = useState("");
+    const timeoutRef = useRef<number | null>(null);
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setActiveButton(e.currentTarget.id);
+        const action =
+            e.currentTarget.id === "delete-completed"
+                ? onClearCompleted
+                : onClearTodoList;
+        timeoutRef.current = setTimeout(() => {
+            action();
+            setActiveButton("");
+        }, 1550);
+    };
+    const handleMouseUp = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setActiveButton("");
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+            timeoutRef.current = null;
+        }
+    };
+
     return (
         <section className="flex gap-2">
             <button
-                onClick={() => onClearCompleted()}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseLeave={handleMouseUp}
+                id="delete-completed"
                 className={cn(
-                    "flex gap-1 relative overflow-hidden transition duration-100 items-center text-sm font-[400] tracking-wide text-neutral-300 hover:text-rose-500 cursor-pointer rounded-xl bg-neutral-800 hover:bg-neutral-800 border border-neutral-700 hover:border-rose-500 py-1.5 px-2.5",
+                    "group flex gap-1 relative overflow-hidden transition duration-100 items-center text-sm font-[400] tracking-wide text-neutral-400 hover:text-rose-600 cursor-pointer rounded-xl bg-neutral-800 hover:bg-neutral-800 border border-neutral-700 hover:border-rose-600 py-1.5 px-2.5",
+                    activeButton === "delete-completed" &&
+                        "hover:text-neutral-50 transition duration-[1500ms]",
                     !hasCompletedTasks &&
                         "hover:border-neutral-700 text-neutral-500 hover:text-neutral-500 opacity-50 cursor-auto"
                 )}
             >
+                <span
+                    className={cn(
+                        "absolute z-0 flex transition-all inset-0",
+                        activeButton === "delete-completed"
+                            ? "hold-to-delete-md"
+                            : "hold-to-delete-mu"
+                    )}
+                ></span>
                 <IconSquareRoundedLetterI
-                    className="rotate-90"
+                    className="rotate-90 z-10"
                     size={20}
                     stroke={1.5}
                 />
-                Delete completed
+                <span className="z-10">Delete completed</span>
             </button>
             <button
-                onClick={() => onClearTodoList()}
-                className="transition duration-100 flex gap-1 items-center text-sm font-[400] tracking-wide text-neutral-300 hover:text-rose-500 cursor-pointer py-1.5 px-2.5 rounded-xl bg-neutral-800 hover:bg-neutral-800 border border-neutral-700 hover:border-rose-500"
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseLeave={handleMouseUp}
+                id="clear-todo-list"
+                className={cn(
+                    "group flex gap-1 relative overflow-hidden transition duration-100 items-center text-sm font-[400] tracking-wide text-neutral-300 hover:text-rose-600 cursor-pointer rounded-xl bg-neutral-800 hover:bg-neutral-800 border border-neutral-700 hover:border-rose-600 py-1.5 px-2.5",
+                    activeButton === "clear-todo-list" &&
+                        "hover:text-neutral-50 transition duration-[1500ms]"
+                )}
             >
-                <IconTrash size={20} stroke={1.5} /> Clear List
+                <span
+                    className={cn(
+                        "absolute z-0 flex transition-all inset-0",
+                        activeButton === "clear-todo-list"
+                            ? "hold-to-delete-md"
+                            : "hold-to-delete-mu"
+                    )}
+                ></span>
+                <IconTrash className="z-10" size={20} stroke={1.5} />
+                <span className="z-10">Clear List</span>
             </button>
         </section>
     );

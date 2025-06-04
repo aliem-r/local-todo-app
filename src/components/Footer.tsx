abc495a6dc7871b41c675680fcb81c2342e01c0d
aliem-r <ali.e.m@outlook.com>
2025-06-04 18:39:24 +0200

    Adjust app title and footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ export default function Footer() {
         <footer className="flex justify-center items-center gap-2 text-sm font-[400] text-neutral-600 py-12">
             <span>Project repo:</span>
             <a
-                href="https://github.com/aliem-r/local-todo-app"
+                href="https://github.com/aliem-r/simple-todo-app"
                 target="_blank"
                 className="text-neutral-600 hover:text-green-400 underline transition duration-100"
             >
-                @aliem-r/local-todo-app
+                @aliem-r/simple-todo-app
             </a>
         </footer>
     );
